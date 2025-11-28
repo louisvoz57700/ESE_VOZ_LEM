3e990afeb013018faaefd7c0f30abd98bfabcd0f
Antoine LEMARIGNIER <ant.lema@outlook.fr>
2025-11-28 17:17:38 +0100

    version_3

---
+++ TP1/Projet_shell/version_3_3/autoradio_L476RG/Core/Src/main.c
@@ -0,0 +1,411 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+#include "dma.h"
+#include "i2c.h"
+#include "sai.h"
+#include "spi.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include "shell.h"
+#include "led.h"
+#include <string.h>
+#include "chip_id.h"
+#include "sgtl5000.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define QUEUE_LENGTH 5
+#define QUEUE_ITEM_SIZE sizeof(uint32_t)
+#define MAX_LEDS 16  // 8 LEDs sur port A + 8 LEDs sur port B
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+QueueHandle_t counterQueue;
+
+#define AUDIO_BUFFER_SIZE 2048
+uint8_t tx_buffer[AUDIO_BUFFER_SIZE]; // Buffer d'émission (vers DAC/Codec)
+uint8_t rx_buffer[AUDIO_BUFFER_SIZE]; // Buffer de réception (depuis ADC/Micro)
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void PeriphCommonClock_Config(void);
+void MX_FREERTOS_Init(void);
+/* USER CODE BEGIN PFP */
+static int fonction(h_shell_t *shell, int argc, char **argv);
+static int addition(h_shell_t *shell, int argc, char **argv);
+static int led_control(h_shell_t *shell, int argc, char **argv);
+void task_shell(void *unused);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+int __io_putchar(int chr)
+{
+	HAL_UART_Transmit(&huart2, (uint8_t*)&chr, 1, HAL_MAX_DELAY);
+	return chr;
+}
+
+static int fonction(h_shell_t *shell, int argc, char **argv)
+{
+	int size;
+	size = snprintf(shell->print_buffer, BUFFER_SIZE, "Je suis une fonction bidon\r\n");
+	shell_uart_write(shell->print_buffer, size);
+	size = snprintf(shell->print_buffer, BUFFER_SIZE, "argc=%d\r\n", argc);
+	shell_uart_write(shell->print_buffer, size);
+	for (int i = 0; i < argc; i++)
+	{
+		size = snprintf(shell->print_buffer, BUFFER_SIZE, "argv[%d]=%s\r\n", i, argv[i]);
+		shell_uart_write(shell->print_buffer, size);
+	}
+	return 0;
+}
+
+static int addition(h_shell_t *shell, int argc, char **argv)
+{
+	int sum = 0;
+	for (int i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]); // atoi conversion chaine de caractère en entier
+	}
+	int size = snprintf(shell->print_buffer, BUFFER_SIZE, "sum=%d\r\n", sum);
+	shell_uart_write(shell->print_buffer, size);
+	return 0;
+}
+
+void task_shell(void *unused)
+{
+	static h_shell_t shell_instance; // Instance statique de la structure
+
+
+	shell_init(&shell_instance);
+	shell_add(&shell_instance, 'f', fonction, "Une fonction inutile");
+	shell_add(&shell_instance, 'a', addition, "Ma super addition");
+	shell_add(&shell_instance, 'l', led_control, "LED control: l <id> <0|1>");
+
+
+	shell_run(&shell_instance); // Contient une boucle infinie
+
+	// Ne sera jamais atteint, mais pour la forme
+	vTaskDelete(NULL);
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if (huart == &huart2)
+	{
+		shell_uart_rx_callback();
+	}
+}
+
+static int led_control(h_shell_t *shell, int argc, char **argv)
+{
+	int numero;
+	int size;
+	numero = atoi(argv[1]);
+	if (argc==3)
+	{
+		if (numero > 8)
+		{
+			Select_LED('B', 16-numero,atoi(argv[2]));
+		}
+		else
+		{
+			int state = atoi(argv[2]);
+			Select_LED('A', numero,atoi(argv[2]));
+		}
+		size = snprintf(shell->print_buffer,BUFFER_SIZE,"c'est fait chef");
+	}
+	else if (argc==4)
+	{
+		numero = atoi(argv[1]) * 10 + atoi(argv[2]);
+		Select_LED('B', 16-numero,atoi(argv[3]));
+		size = snprintf(shell->print_buffer,BUFFER_SIZE,"c'est fait chef");
+	}
+	else
+	{
+		size = snprintf(shell->print_buffer,BUFFER_SIZE,"Il y a pas assez de led");
+	}
+	drv_uart1_transmit(shell->print_buffer,size);
+	return 0;
+}
+
+void Generate_Triangle_Wave(uint8_t *buffer, uint32_t sizeInBytes) {
+    int16_t *pSample = (int16_t *)buffer;
+
+    // 16 bits = 2 octets. Stereo = 2 canaux.
+    // Donc 1 "Frame" audio (Gauche + Droite) = 4 octets.
+    uint32_t totalFrames = sizeInBytes / 4;
+
+    // On divise le buffer en deux phases : Montée et Descente
+    uint32_t halfPeriod = totalFrames / 2;
+
+    int16_t amplitude = 15000;
+    float step = (float)(amplitude * 2) / (float)halfPeriod;
+    float currentVal = -15000.0f;
+
+    // Phase 1 : Montée
+    for (uint32_t i = 0; i < halfPeriod; i++) {
+        int16_t val = (int16_t)currentVal;
+
+        pSample[i * 2]     = val; // Canal Gauche
+        pSample[i * 2 + 1] = val; // Canal Droit (identique pour du mono centré)
+
+        currentVal += step;
+    }
+
+    // Phase 2 : Descente
+    currentVal = 15000.0f;
+    for (uint32_t i = halfPeriod; i < totalFrames; i++) {
+        int16_t val = (int16_t)currentVal;
+
+        pSample[i * 2]     = val;
+        pSample[i * 2 + 1] = val;
+
+        currentVal -= step;
+    }
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Configure the peripherals common clocks */
+  PeriphCommonClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USART2_UART_Init();
+  MX_SPI3_Init();
+  MX_I2C2_Init();
+  MX_SAI2_Init();
+  /* USER CODE BEGIN 2 */
+	MCP23S17_Init();
+	MCP23S17_SetAllPinsHigh();
+
+	__HAL_SAI_ENABLE(&hsai_BlockA2); // pour horloge pour codec
+
+	printf("Bonjour Antonio y Louis\r\n");
+
+	Lire_CHIP_ID(); // le printf est dans la fonction
+
+
+	Generate_Triangle_Wave(tx_buffer, AUDIO_BUFFER_SIZE);
+
+	if (HAL_SAI_Transmit_DMA(&hsai_BlockA2, tx_buffer, AUDIO_BUFFER_SIZE/ 2) != HAL_OK)
+	{
+		printf("Erreur demarrage SAI TX DMA\r\n");
+		Error_Handler();
+	}
+
+	if (HAL_SAI_Receive_DMA(&hsai_BlockB2, rx_buffer, AUDIO_BUFFER_SIZE/ 2) != HAL_OK)
+	{
+		printf("Erreur demarrage SAI RX DMA\r\n");
+		Error_Handler();
+	}
+
+	// Création de la queue
+	counterQueue = xQueueCreate(QUEUE_LENGTH, QUEUE_ITEM_SIZE);
+	if (counterQueue == NULL) {
+		Error_Handler();
+	}
+
+	// Création de la tâche shell
+	if (xTaskCreate(task_shell, "Shell", 512, NULL, 1, NULL) != pdPASS)
+	{
+		printf("Error creating task Shell\r\n");
+		Error_Handler();
+	}
+
+  /* USER CODE END 2 */
+
+  /* Call init function for freertos objects (in cmsis_os2.c) */
+  MX_FREERTOS_Init();
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	while (1)
+	{
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	}
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 10;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief Peripherals Common Clock Configuration
+  * @retval None
+  */
+void PeriphCommonClock_Config(void)
+{
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the peripherals clock
+  */
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_SAI2;
+  PeriphClkInit.Sai2ClockSelection = RCC_SAI2CLKSOURCE_PLLSAI1;
+  PeriphClkInit.PLLSAI1.PLLSAI1Source = RCC_PLLSOURCE_HSI;
+  PeriphClkInit.PLLSAI1.PLLSAI1M = 1;
+  PeriphClkInit.PLLSAI1.PLLSAI1N = 13;
+  PeriphClkInit.PLLSAI1.PLLSAI1P = RCC_PLLP_DIV17;
+  PeriphClkInit.PLLSAI1.PLLSAI1Q = RCC_PLLQ_DIV2;
+  PeriphClkInit.PLLSAI1.PLLSAI1R = RCC_PLLR_DIV2;
+  PeriphClkInit.PLLSAI1.PLLSAI1ClockOut = RCC_PLLSAI1_SAI1CLK;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1)
+	{
+	}
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+	/* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
